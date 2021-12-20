9c13e19f1baedf627098ef759c1a4caa6d585fa5
Affou siata <affousiatao59@gmail.com>
2021-12-20 09:11:11 +0000

    mon deuxieme commit

--- app1.js
+++ app1.js
@@ -8,7 +8,7 @@ function affiche  (arg){
  
 function del(){
     console.log();
-    ecran.innerHTML= ecran.innerHTML.substring(0,exp.length-1)
+    ecran.innerHTML= ecran.innerHTML.slice(0,-1);
 }
 function supp (ac){
     ecran.innerHTML ="";
@@ -17,6 +17,47 @@ function calcul() {
     console.log();
     ecran.innerHTML = eval(ecran.innerHTML);
 }
+function log (){
+	ecran.innerHTML = Math.log(ecran.innerHTML);
+}
+function cosinus(){
+	ecran.innerHTML = Math.cos(ecran.innerHTML);
+}
+
+function sinus(){
+	ecran.innerHTML = Math.sin(ecran.innerHTML);
+}
+function tangente(){
+	ecran.innerHTML = Math.tan(ecran.innerHTML);
+}
+function sqrt(){
+	ecran.innerHTML = Math.sqrt(ecran.innerHTML,2);
+}
+function carre(){
+	ecran.innerHTML = Math.pow(ecran.innerHTML,2);
+}
+function ln(){
+	ecran.innerHTML = Math.ln(ecran.innerHTML);
+}
+/*
+function zert(){
+	ecran.innerHTML = Math.
+}
+
+function absolue(){
+	ecran.innerHTML = Math.
+}
+
+function on(){
+	ecran.innerHTML = Math.
+}
+function exp(){
+	ecran.innerHTML = Math.
+}
+function second(){
+	ecran.innerHTML = Math.
+}
+*/
 
 
 
